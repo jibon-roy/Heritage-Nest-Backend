fd9cd2c0a6a2ad961e84c914ae85e75880827f32
Jibon Krishna Roy <mailme.jibon@gmail.com>
2024-08-13 15:48:21 +0600

    user schema updated

--- src/schema/user.js
+++ src/schema/user.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 const userSchema = new mongoose.Schema({
   name: {
     type: String,
-    required: false,
+    required: true,
   },
   email: {
     type: String,
@@ -13,14 +13,23 @@ const userSchema = new mongoose.Schema({
   dateOfBirth: {
     type: String,
     require: false,
+    default: null,
   },
   gender: {
     type: String,
     require: false,
+    default: null,
   },
   password: {
     type: String,
     required: false,
+    default: null,
+  },
+  role: {
+    type: String,
+    required: true,
+    enum: ["admin", "bidder", "property_owner"],
+    default: "bidder",
   },
 });
 export default userSchema;
